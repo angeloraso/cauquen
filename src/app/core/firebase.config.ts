54306b11490c94720c4a5ca93563807b5a4e9985
Angelo Raso <araso@anura.com.ar>
2024-04-01 20:05:27 -0300

    Delete duplicated file

--- src/app/core/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyDiu5Z9f3R_18o8g3KMiMKl6qtRYVjj_TI',
-  authDomain: 'cauquen-7f101.firebaseapp.com',
-  projectId: 'cauquen-7f101',
-  storageBucket: 'cauquen-7f101.appspot.com',
-  messagingSenderId: '1094121137684',
-  appId: '1:1094121137684:web:c1d1ed7b2597414f9ce901'
-};
