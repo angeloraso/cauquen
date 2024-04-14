08c6116da4b0b3340688d77d9884b5cd76247ca3
Angelo Raso <araso@anura.com.ar>
2024-04-14 11:19:44 -0300

    Update authDomain

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,6 +1,6 @@
 export const config = {
   apiKey: 'AIzaSyDiu5Z9f3R_18o8g3KMiMKl6qtRYVjj_TI',
-  authDomain: 'cauquen.firebaseapp.com',
+  authDomain: 'cauquen-7f101.firebaseapp.com',
   projectId: 'cauquen-7f101',
   storageBucket: 'cauquen-7f101.appspot.com',
   messagingSenderId: '1094121137684',
